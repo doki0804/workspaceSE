44283524107b7a47c734201df2072297509e2045
이원호 <doki0804@naver.com>
2022-12-05 11:51:07 +0900

    ...

--- 20.입출력[IO]/src/bytestream/FileInputStreamMain.java
+++ 20.입출력[IO]/src/bytestream/FileInputStreamMain.java
@@ -1,9 +1,36 @@
 package bytestream;
+/*
+ * <<입력스트림(InputStream)사용방법>>
+ * 1. 데이타 source 선정(파일)
+ * 2. 입력스트림(InputStream)객체생성(FileInputStream)
+ * 3. 입력스트림(InputStream)으로부터 한바이트씩  읽는다(read)
+ * 4. 입력스트림(InputStream)닫는다.(close) 
+ */
+import java.io.FileInputStream;
 
 public class FileInputStreamMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
+		FileInputStream fis = new FileInputStream("fileOut.dat");
+		int readByte = fis.read();
+		System.out.println("1.byte:"+Integer.toBinaryString(readByte));
+		readByte = fis.read();
+		System.out.println("2.byte:"+Integer.toBinaryString(readByte));
+		readByte = fis.read();
+		System.out.println("3.byte:"+Integer.toBinaryString(readByte));
+		readByte = fis.read();
+		System.out.println("4.byte:"+Integer.toBinaryString(readByte));
+		int byteCount=0;
+		while(true) {
+			readByte=fis.read();
+			if(readByte==-1)
+				break;
+			byteCount++;
+			System.out.println(Integer.toBinaryString(readByte));
+		}
+		fis.close();
+		System.out.println("FileInputStream.read()<-----fileOut.dat: "+byteCount+"바이트 읽음");
 		
 		
 		
