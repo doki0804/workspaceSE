35db2c9a325372b15436ef19f43ae5ccdcc7d074
이원호 <doki0804@naver.com>
2022-12-07 11:12:00 +0900

    ...

--- 21.쓰레드[Thread]/src/ControlSleepThread.java
+++ 21.쓰레드[Thread]/src/ControlSleepThread.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 
 public class ControlSleepThread extends Thread{
 
@@ -7,6 +8,9 @@ public class ControlSleepThread extends Thread{
 		while(true) {
 			try {
 				System.out.println("ControlSleepThread --> 1초마다 1번씩 시간출력");
+				Date now = new Date();
+				System.out.println(now.toString());
+				
 				/*
 				 * 현재실행되고 있는 쓰레드를 1000ms동안 실행중지
 				 */
--- 21.쓰레드[Thread]/src/ControlSleepThreadMain.java
+++ 21.쓰레드[Thread]/src/ControlSleepThreadMain.java
@@ -1,8 +1,11 @@
 
 public class ControlSleepThreadMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("1.main");
+		System.out.println("2.main 쓰레드 3초간 sleep");
+		Thread.sleep(3000);
+		System.out.println("3.main 쓰레드 3초후 깨어나고 실행");
 		ControlSleepThread cst = new ControlSleepThread();
 		cst.start();
 		System.out.println("9.main return");
