8259a9b2301908d513c30aaf338fafe8840c28b3
이원호 <doki0804@naver.com>
2022-12-08 12:15:46 +0900

    ..

--- 22.GUI[swing]/src/basic/ChildJFrameWB.java
+++ 22.GUI[swing]/src/basic/ChildJFrameWB.java
@@ -5,10 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class ChildJFrameWB extends JFrame {
 
 	private JPanel contentPane;
+	private JButton okBtn;
+	private JButton cancleBtn;
 
 	/**
 	 * Launch the application.
@@ -30,12 +34,22 @@ public class ChildJFrameWB extends JFrame {
 	 * Create the frame.
 	 */
 	public ChildJFrameWB() {
+		setTitle("나의첫번째WB프레임");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 300, 380);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 128, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		okBtn = new JButton("확인");
+		okBtn.setBounds(51, 42, 65, 54);
+		contentPane.add(okBtn);
+		
+		cancleBtn = new JButton("취소");
+		cancleBtn.setBounds(165, 42, 65, 54);
+		contentPane.add(cancleBtn);
 	}
-
 }
