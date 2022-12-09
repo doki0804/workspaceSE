59f561fb26791e9f33ed85081901d86178e7e2f9
이원호 <doki0804@naver.com>
2022-12-09 12:08:31 +0900

    ..

--- 23.내부클래스[NastedClass]/src/OuterComposingInnerClass.java
+++ 23.내부클래스[NastedClass]/src/OuterComposingInnerClass.java
@@ -4,6 +4,9 @@ public class OuterComposingInnerClass {
 	public void outer_member_method() {
 		System.out.println("OuterComposingInnercalss.outer_member_method()");
 	}
-	
+	/**********Outer클래스에서 Inner클래스 사용**********/
+	public void outer_inner_class_use() {
+		
+	}
 	
 }
