8aef2f7cc4dfcbd990f50de5d1596ba8f85752d0
이원호 <doki0804@naver.com>
2022-12-26 16:49:20 +0900

    .

--- 25.JDBC[Java DataBase Connectivity]/src/basic/01.address_insert_update_delete_select.sql
+++ 25.JDBC[Java DataBase Connectivity]/src/basic/01.address_insert_update_delete_select.sql
@@ -1,19 +1,28 @@
 --insert
-insert into address values(address_no_SEQ.nextval,'김경호','123-4567','경기도 성남시');
-insert into address values(address_no_SEQ.nextval,'김경우','123-4567','경기도 성남시');
-insert into address values(address_no_SEQ.nextval,'김경미','123-4567','경기도 성남시');
+insert into address values(address_no_seq.nextval,'김경호','123-4568','경기도 성남시');
+insert into address values(address_no_seq.nextval,'박경호','123-4568','경기도 구리시');
+insert into address values(address_no_seq.nextval,'최경호','123-4568','경기도 포천시');
+insert into address values(address_no_seq.nextval,'구경호','123-4568','경기도 이천시');
+insert into address values(address_no_seq.nextval,'정경호','123-4568','경기도 군포시');
+insert into address values(address_no_seq.nextval,'이경호','123-4568','경기도 안양시');
+insert into address values(address_no_seq.nextval,'주경호','123-4568','경기도 안산시');
+insert into address values(address_no_seq.nextval,'양경호','123-4568','경기도 인천시');
+insert into address values(address_no_seq.nextval,'인경호','123-4568','경기도 시흥시');
+
+
+--insert
+insert into address values(address_no_seq.nextval ,'김경호','123-4568','경기도 성남시');
+commit;
 
 --update(pk update)
-update address set name = '김변경',phone='899-9900',address='서울시 강남구' where no=1;
+update address set name='김경호',phone='899-9999',address='서울시 강남구' where no = 1;
+commit;
+
+--delete (pk delete)
+delete address where no=1;
 
 
---delete(pk delete)
-
-
-
---select(pk select)
-
-
-
+--select (pk select)
+select no,name,phone,address from address where no = 3;
 --select(all select)
-select no,name,phone,address from address;
+select no,name,phone,address from address; 
