2dca0076e605c26ea94ffdd37d9c4de26f486d2b
이원호 <doki0804@naver.com>
2022-12-27 14:17:17 +0900

    .

---
+++ 25.JDBC[Java DataBase Connectivity]/src/dao/address/third/Address.java
@@ -0,0 +1,60 @@
+package dao.address.third;
+/*
+ * VO(Value Object),DTO(Date Transfer Object)
+ *   - address테이블 1개 row의 데이터의 값을 가지는 객체
+ *   - address테이블 1개 row의 데이터의 값을 전달(파라메타,리턴데이터)시키기 위한 객체
+ *   - address테이블의 컬럼과 동일한수의 멤버필드를 갖는다.
+ */
+/*
+ 이름      널?       유형            
+------- -------- ------------- 
+NO               NUMBER(4)     
+NAME    NOT NULL VARCHAR2(50)  
+PHONE            VARCHAR2(50)  
+ADDRESS          VARCHAR2(100) 
+ */
+public class Address {
+	private int no;
+	private String name;
+	private String phone;
+	private String address;
+
+	public Address() {
+	}
+	public Address(int no, String name, String phone, String address) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+	}
+	public int getNo() {
+		return no;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Address [no=" + no + ", name=" + name + ", phone=" + phone + ", address=" + address + "]";
+	}
+	
+}
--- 25.JDBC[Java DataBase Connectivity]/src/dao/address/third/AddressDao3TestMain.java
+++ 25.JDBC[Java DataBase Connectivity]/src/dao/address/third/AddressDao3TestMain.java
@@ -4,20 +4,20 @@ public class AddressDao3TestMain {
 
 	public static void main(String[] args) throws Exception {
 		/*
-		 * AddressDao2TestMain에서
-		 * AddressDao2로 파라메타를 전달
+		 * AddressDao3TestMain에서
+		 * AddressDao3로 파라메타객체(DTO,VO)를 전달
 		 */
-		AddressDao3 addressDao2 = new AddressDao3();
+		AddressDao3 addressDao3 = new AddressDao3();
 		System.out.println("1.insert");
-		addressDao2.insert("이소라","343-9090","경기도민");
+		addressDao3.insert("이소라","343-9090","경기도민");
 		System.out.println("2.update");
-		addressDao2.update(16,"삼소라", "888-8888", "대전시 중구");
+		addressDao3.update(16,"삼소라", "888-8888", "대전시 중구");
 		System.out.println("3.delete");
-		addressDao2.delete(16);
+		addressDao3.delete(16);
 		System.out.println("4.findByPrimaryKey");
-		addressDao2.findByPrimaryKey(10);
+		addressDao3.findByPrimaryKey(10);
 		System.out.println("5.findAll");
-		addressDao2.findAll();
+		addressDao3.findAll();
 	}
 
 }
