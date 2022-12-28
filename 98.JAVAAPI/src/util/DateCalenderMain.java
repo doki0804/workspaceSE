b89d1ccb4585eb32b5eb0f9314c3e58182ddea71
이원호 <doki0804@naver.com>
2022-12-28 16:25:14 +0900

    .

--- 98.JAVAAPI/src/util/DateCalenderMain.java
+++ 98.JAVAAPI/src/util/DateCalenderMain.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateCalenderMain {
@@ -53,6 +54,61 @@ public class DateCalenderMain {
 		System.out.println(myBirthDay.toString());
 		System.out.println(myBirthDay.toLocaleString());
 		System.out.println(myBirthDay.getTime());
+		
+		sdf2.applyPattern("yyyy/MM/dd HH:mm:ss");
+		Date yourBirthDay=sdf2.parse("1998/05/12 17:45:13");
+		System.out.println(yourBirthDay.toLocaleString());
+		System.out.println(yourBirthDay.getTime());
+		
+		/*************************java.util.Calendar*************************/
+		Calendar cal1 = Calendar.getInstance();
+		System.out.println(cal1);
+		System.out.println(cal1.getCalendarType());
+		
+		long currentTimeMillis3 = cal1.getTimeInMillis();
+		System.out.println("cal1.getTimeInMillis()==>"+currentTimeMillis3);
+		int y = cal1.get(Calendar.YEAR);
+		int month = cal1.get(Calendar.MONTH)+1;
+		int day = cal1.get(Calendar.DAY_OF_MONTH);
+		int hour = cal1.get(Calendar.HOUR);
+		int min = cal1.get(Calendar.MINUTE);
+		int second = cal1.get(Calendar.SECOND);
+		int msecond = cal1.get(Calendar.MILLISECOND);
+		System.out.println(y+"/"+month+"/"+day+" "+hour+":"+min+":"+second+":"+msecond);
+		
+		System.out.println("---------Calendar-->Date---------");
+		Calendar cal2 = Calendar.getInstance();
+		cal2.set(2021, Calendar.JANUARY, 10, 13, 59, 59);
+		System.out.println("Calendar cal2 --> "+cal2);
+		Date date2 = cal2.getTime();
+		System.out.println("Date date2-->"+date2.toLocaleString());
+		
+		System.out.println("---------Date-->Calendar---------");
+		Date date3 = new Date();
+		Calendar cal3 = Calendar.getInstance();
+		cal3.setTimeInMillis(date3.getTime());
+		System.out.println("Date date3    -->"+date3.toLocaleString());
+		System.out.println("Calender cal3 -->"+cal3);
+		
+		System.out.println("----------Date객체매쏘드----------");
+		Date date4 = new SimpleDateFormat("yyyy-MM-dd").parse("2022-01-10");
+		Date date5 = new SimpleDateFormat("yyyy-MM-dd").parse("2022-01-10");
+		Date date6 = new SimpleDateFormat("yyyy-MM-dd").parse("2022-01-15");
+		
+		System.out.println("equals date4 == date5:"+date4.equals(date5));
+		System.out.println("equals date4 == date6:"+date4.equals(date6));
+		System.out.println("compareTo:"+date4.compareTo(date5));
+		System.out.println("compareTo:"+date4.compareTo(date6));
+		System.out.println("compareTo:"+date6.compareTo(date4));
+		long gapMillisec = date6.getTime()-date4.getTime();
+		System.out.println("gapMillisec :"+gapMillisec);
+		System.out.println("min :"+gapMillisec/1000/60);
+		System.out.println("hour :"+gapMillisec/1000/60/60);
+		System.out.println("day :"+gapMillisec/1000/60/60/24);
+		
+		System.out.println("----------java.util.Date-->java.sql.Date----------");
+		System.out.println("----------java.sql.Date-->java.util.Date----------");
+		
 		
 		
 		
