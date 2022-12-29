5862410fc17156d9f7cf0d236a4e20a9ae0aa360
이원호 <doki0804@naver.com>
2022-12-29 12:22:07 +0900

    .

--- 25.JDBC[Java DataBase Connectivity]/src/resultset/ResultSetMain.java
+++ 25.JDBC[Java DataBase Connectivity]/src/resultset/ResultSetMain.java
@@ -1,22 +1,17 @@
 package resultset;
 
+import java.sql.Connection;
+
+import dao.common.DataSource;
+
 public class ResultSetMain {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		DataSource dataSource = new DataSource();
+		Connection con = dataSource.getConnection();
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+			
 		
 		
 		
