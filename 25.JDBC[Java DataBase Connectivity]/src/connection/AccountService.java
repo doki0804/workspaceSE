2a6829e43b4a411dc2bcbedf022a1132611a2af0
이원호 <doki0804@naver.com>
2022-12-29 16:24:24 +0900

    .

--- 25.JDBC[Java DataBase Connectivity]/src/connection/AccountService.java
+++ 25.JDBC[Java DataBase Connectivity]/src/connection/AccountService.java
@@ -2,6 +2,7 @@ package connection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import dao.common.DataSource;
 
@@ -14,7 +15,7 @@ public class AccountService {
 	 * 모든계좌의 잔고를 balance만큼 갱신
 	 */
 	public void updateBalance(int balance) {
-		
+		boolean exceptionCondition = true;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		String updateSql="update accounta set acc_balance = acc_balance + ? where acc_no = ?";
@@ -23,14 +24,67 @@ public class AccountService {
 			System.out.println("-----------updateBalacnce transaction start-----------");
 			con.setAutoCommit(false);
 			pstmt = con.prepareStatement(updateSql);
-			pstmt.setInt(1, balance);
-			pstmt.setInt(2, balance);
-			pstmt.executeUpdate();
 			
+			/*********************10000번 계좌 update*********************/
+			int acc_no=10000;
+			pstmt.setInt(1, balance);
+			pstmt.setInt(2, acc_no);
+			int rowCount = pstmt.executeUpdate();
+			System.out.println(acc_no+"번 계좌 "+rowCount+" update");
+			/*********************20000번 계좌 update*********************/
+			acc_no=20000;
+			pstmt.setInt(1, balance);
+			pstmt.setInt(2, acc_no);
+			rowCount = pstmt.executeUpdate();
+			System.out.println(acc_no+"번 계좌 "+rowCount+" update");
+			/*********************30000번 계좌 update*********************/
+			acc_no=30000;
+			pstmt.setInt(1, balance);
+			pstmt.setInt(2, acc_no);
+			rowCount = pstmt.executeUpdate();
+			System.out.println(acc_no+"번 계좌 "+rowCount+" update");
+			
+			/**예외발생**/
+			int rn = (int)(Math.random()*2);
+			if(rn%2==0) {
+				exceptionCondition = true;
+			}else {
+				exceptionCondition = false;
+			}
+			if(exceptionCondition) {
+				throw new Exception("모든계좌잔고갱신예외발생");
+			}
+			 
+			/*********************40000번 계좌 update*********************/
+			acc_no=40000;
+			pstmt.setInt(1, balance);
+			pstmt.setInt(2, acc_no);
+			rowCount = pstmt.executeUpdate();
+			System.out.println(acc_no+"번 계좌 "+rowCount+" update");
+			/*********************50000번 계좌 update*********************/
+			acc_no=50000;
+			pstmt.setInt(1, balance);
+			pstmt.setInt(2, acc_no);
+			rowCount = pstmt.executeUpdate();
+			System.out.println(acc_no+"번 계좌 "+rowCount+" update");
+			/*********************60000번 계좌 update*********************/
+			acc_no=60000;
+			pstmt.setInt(1, balance);
+			pstmt.setInt(2, acc_no);
+			rowCount = pstmt.executeUpdate();
+			System.out.println(acc_no+"번 계좌 "+rowCount+" update");
+			System.out.println("-----------updateBalacnce transaction end[commit]-----------");
+			con.commit();
 			
 			
 		}catch (Exception e) {
-			
+			System.err.println("99.예외발생:"+e.getMessage());
+			System.out.println("-----------updateBalacnce transaction end[rollback]-----------");
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 		}finally {
 			/*
 			 * exception 발생유무와 관계없이 항상실행
--- 25.JDBC[Java DataBase Connectivity]/src/connection/AccountServiceTransactionMain.java
+++ 25.JDBC[Java DataBase Connectivity]/src/connection/AccountServiceTransactionMain.java
@@ -1,13 +1,13 @@
 package connection;
 
-public class AccountServiceMain {
+public class AccountServiceTransactionMain {
 
 	public static void main(String[] args) {
 		AccountService accountService = new AccountService();
 		/*
 		 * 모든잔고 update 트랜젝션
 		 */
-		accountService.updateBalance(3333);
+		accountService.updateBalance(1);
 		
 	}
 }
